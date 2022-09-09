c1bcd30647ff9b0dde2b653cbdb31d475e3b926d
Tibe_dev <77735351+Tibe52@users.noreply.github.com>
2022-09-09 17:30:24 +0200

    Update oauth.js

--- func/oauth.js
+++ func/oauth.js
@@ -1,15 +1,15 @@
-import crypto from "crypto";
-
-export async function handler(event, context) {
+exports.handler = async function (event, context) {
     const redirectUri = new URL("/.netlify/functions/oauth-callback", DEPLOY_PRIME_URL);
+    let url = `https://discord.com/api/oauth2/authorize?client_id=${encodeURIComponent(process.env.DISCORD_CLIENT_ID)}&redirect_uri=${encodeURIComponent(redirectUri)}&response_type=code&scope=identify&prompt=none`;
 
-    const state = crypto.randomBytes(25).toString("hex");
+    if (event.queryStringParameters.state !== undefined) {
+        url += `&state=${encodeURIComponent(event.queryStringParameters.state)}`;
+    }
 
     return {
         statusCode: 303,
         headers: {
-            "Location": `https://discord.com/api/oauth2/authorize?client_id=${encodeURIComponent(process.env.DISCORD_CLIENT_ID)}&redirect_uri=${encodeURIComponent(redirectUri)}&response_type=code&scope=identify&prompt=none&state=${state}`,
-            "Set-Cookie": `__Secure-CSRFState=${state}; Domain=${DEPLOY_PRIME_URL.replace("https://", "")}; Path=/.netlify/functions/oauth-callback; Secure; HttpOnly; SameSite=Lax`
+            "Location": url
         }
     };
 }
