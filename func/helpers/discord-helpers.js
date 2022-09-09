667851ee73f6e1e302f07e601a353a98425d1e46
Tibe_dev <77735351+Tibe52@users.noreply.github.com>
2022-09-09 17:31:54 +0200

    Update discord-helpers.js

--- func/helpers/discord-helpers.js
+++ func/helpers/discord-helpers.js
@@ -1,3 +1,5 @@
-export const API_ENDPOINT = "https://discord.com/api/v9";
-export const MAX_EMBED_FIELD_CHARS = 1024;
-export const MAX_EMBED_FOOTER_CHARS = 2048;
+const API_ENDPOINT = "https://discord.com/api/v9";
+const MAX_EMBED_FIELD_CHARS = 1024;
+const MAX_EMBED_FOOTER_CHARS = 2048;
+
+module.exports = { API_ENDPOINT, MAX_EMBED_FIELD_CHARS, MAX_EMBED_FOOTER_CHARS };
